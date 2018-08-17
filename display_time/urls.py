4e13ebffae0ee36361b6df6eaa654beeb5fb335e
Angela Leighton <angela@Angelas-MacBook-Pro.local>
2018-08-17 16:00:09 -0500

    first commit

---
+++ display_time/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.urls import path, include
+# Notice we added include  
+# comment out or delete the line below, we will not be using the admin module
+# from django.contrib import admin
+# Why won't we use the admin module? Because we're focusing on how to code, 
+# not on how to help people who do not know how to code how to manage the app.
+urlpatterns = [
+    path('', include('apps.display.urls')) # And now we use the include function to pull in our app.urls...
+]
